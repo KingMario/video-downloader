bf53c588938b5c5d198caa8d4aa500613c96ea0f
Changyu Geng <gcyyq@hotmail.com>
2024-03-10 10:24:01 +0800

    Refactor content.ts to improve code readability and maintainability

--- src/content.ts
+++ src/content.ts
@@ -3,18 +3,22 @@ import type { VideoInfo } from "./interfaces";
 document.addEventListener("DOMContentLoaded", function () {
   let message: VideoInfo | null = null;
 
-  const observer = new MutationObserver(function () {
-    const title = (
-      document.querySelector(".classroom-nav__details h1") as HTMLElement
-    )?.innerText.replace(/:/g, " - ");
+  const processName = (str?: string) =>
+    str
+      ? str
+          .replace(/[:/\\]/g, " - ")
+          .replace(/[?"]/g, "")
+          .replace(/\s+/g, " ")
+      : undefined;
 
-    const sectionName = (
-      document.querySelector(
-        "section.classroom-toc-section:has(.classroom-toc-item--selected) h2 button"
-      ) as HTMLElement
-    )?.innerText
-      .replace(/:/g, " - ")
-      .replace(/\?/g, "");
+  const getNameBySelector = (selector: string) =>
+    processName((document.querySelector(selector) as HTMLElement)?.innerText);
+
+  const observer = new MutationObserver(function () {
+    const title = getNameBySelector(".classroom-nav__details h1");
+    const sectionName = getNameBySelector(
+      "section.classroom-toc-section:has(.classroom-toc-item--selected) h2 button"
+    );
 
     const tocItemSelected = document.querySelector(
       ".classroom-toc-item--selected"
@@ -24,13 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
       Array.from(tocItemSelected?.parentElement!.children).indexOf(
         tocItemSelected
       ) + 1;
-
-    const videoName = (
-      document.querySelector(".classroom-nav__details h2") as HTMLElement
-    )?.innerText
-      .replace(/:/g, " - ")
-      .replace(/\?/g, "");
-
+    const videoName = getNameBySelector(".classroom-nav__details h2");
     const videoUrl = document.querySelector("video")?.getAttribute("src");
 
     let newMessage: VideoInfo | null = null;
